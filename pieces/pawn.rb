5f7f809fb5b018f15c51cea10aebbf2ca461b947
Bill Wiegert <billsteh@gmail.com>
2017-05-17 10:57:41 -0400

    Correct pawn attack direction

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -1,27 +1,50 @@
 require_relative 'piece'
-require_relative 'stepping_module'
 
 class Pawn < Piece
-  include SteppingPiece
+
   def initialize(color, board, pos = nil)
     @symbol = define_symbol("♟")
-    super(color, board)
+    super(color, board, pos)
   end
 
-  def move_dirs
-    dirs = []
-    if @color == :white
-      dirs << [-1, 0]
-      if @pos[0] == 6
-        dirs << [-2, 0]
-      end
-    else
-      dirs << [1, 0]
-      if @pos[0] == 1
-        dirs << [2,0]
-      end
+  def moves
+    forward_moves + diagonal_attacks
+  end
+
+  private
+
+  def forward_dir
+    (color == :white) ? -1 : 1
+  end
+
+  def at_start_row?
+    (color == :white) ? (pos[0] == 6) : (pos[0] == 1)
+  end
+
+  def forward_moves
+    x, y = pos
+    one_step = [x + forward_dir, y]
+
+    return [] unless board.valid_pos?(one_step) && board.empty?(one_step)
+
+    valid_moves = [one_step]
+
+    if at_start_row?
+      two_step = [x + (forward_dir * 2), y]
+      valid_moves << two_step if board.empty?(two_step)
     end
 
-    return dirs
-   end
+    valid_moves
+  end
+
+  def diagonal_attacks
+    x, y = pos
+
+    diagonal_moves = [[x + forward_dir, y - 1], [x + forward_dir, y + 1]]
+
+    diagonal_moves.select do |end_pos|
+      board.valid_pos?(end_pos) && !board.empty?(end_pos) &&
+      board[end_pos].color != color
+    end
+  end
 end
