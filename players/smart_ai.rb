33cc09203d4550410afee40a8919904abf1a8a7f
Bill Wiegert <billsteh@gmail.com>
2017-06-27 15:29:41 -0400

    Add end game piece-square table for kings

--- players/smart_ai.rb
+++ players/smart_ai.rb
@@ -78,10 +78,22 @@ class SmartAI < Player
     [ 20, 30, 10,  0,  0, 10, 30, 20]
   ]
 
+  WHITE_KING_PS_END = [
+    [-50,-40,-30,-20,-20,-30,-40,-50],
+    [-30,-20,-10,  0,  0,-10,-20,-30],
+    [-30,-10, 20, 30, 30, 20,-10,-30],
+    [-30,-10, 30, 40, 40, 30,-10,-30],
+    [-30,-10, 30, 40, 40, 30,-10,-30],
+    [-30,-10, 20, 30, 30, 20,-10,-30],
+    [-30,-30,  0,  0,  0,  0,-30,-30],
+    [-50,-30,-30,-30,-30,-30,-30,-50]
+  ]
+
   BLACK_PAWN_PS = WHITE_PAWN_PS.reverse
   BLACK_KNIGHT_PS = WHITE_KNIGHT_PS.reverse
   BLACK_BISHOP_PS = WHITE_BISHOP_PS.reverse
   BLACK_ROOK_PS = WHITE_ROOK_PS.reverse
   BLACK_QUEEN_PS = WHITE_QUEEN_PS.reverse
   BLACK_KING_PS = WHITE_KING_PS.reverse
+  BLACK_KING_PS_END = WHITE_KING_PS_END.reverse
 end
